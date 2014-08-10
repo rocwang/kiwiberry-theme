a32eb5d99dcd2d0ced6f72d311c510694e5e9829
Roc Wang <roc@moustacherepublic.com>
2014-08-10 23:13:59 +1200

    Set up front end work flow.

--- gulpfile.js
+++ gulpfile.js
@@ -1,151 +1,149 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) 2011-2014 Webcomm Pty Ltd
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+// Base paths
+var paths = {
+  src   : './',
+  vendor: './bower_components/',
+  dest  : './test/skin/frontend/kiwifruit/default/'
+};
+
+// Assets of Kiwifruit
+var appFiles = {
+  styles : [
+    paths.src + 'scss/kiwifruit.scss'
+  ],
+  scripts: [
+    paths.src + 'js/kiwifruit.js',
+    paths.src + 'js/noconflict.js'
+  ],
+  images : paths.src + 'images/*'
+};
+
+// Vendor assets
+var vendorFiles = {
+  styles : [
+    paths.vendor + 'animate.css/animate.css'
+  ],
+  fonts: [
+    paths.vendor + 'bootstrap-sass-official/assets/fonts/bootstrap/*',
+    paths.vendor + 'font-awesome/fonts/*',
+  ],
+  scripts: [
+    paths.vendor + 'jquery/dist/jquery.js',
+    paths.vendor + 'bootstrap-sass-official/assets/javascripts/bootstrap.js',
+    paths.vendor + 'modernizr/modernizr.js',
+    paths.vendor + 'underscore/underscore.js'
+  ]
+};
 
 // Load plugins
-var
-  gulp         = require('gulp'),
-  less         = require('gulp-less'),
-  minifycss    = require('gulp-minify-css'),
-  uglify       = require('gulp-uglify'),
-  rimraf       = require('gulp-rimraf'),
-  concat       = require('gulp-concat'),
-  notify       = require('gulp-notify'),
-  cache        = require('gulp-cache'),
-  livereload   = require('gulp-livereload');
+var gulp = require('gulp');
+var util = require('gulp-util');
+var cache = require('gulp-cache');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
 
-var config = {
-
-  // If you do not have the live reload extension installed,
-  // set this to true. We will include the script for you,
-  // just to aid with development.
-  appendLiveReload: true,
-
-  // Should CSS & JS be compressed?
-  minifyCss: true,
-  uglifyJS: true
-
+// Allows gulp --dev to be run for a more verbose output
+var isProduction = false;
+if(util.env.dist === true) {
+  isProduction = true;
 }
 
 // CSS
-gulp.task('css', function() {
-  var stream = gulp
-    .src('src/less/style.less')
-    .pipe(less().on('error', notify.onError(function (error) {
-      return 'Error compiling LESS: ' + error.message;
-    })))
-    .pipe(gulp.dest('css'));
+gulp.task('css', function(){
+  var sass = require('gulp-ruby-sass');
+  var es = require('event-stream');
+  var autoprefixer = require('gulp-autoprefixer');
+  var combineMediaQueries = require('gulp');
+  var cssmin = require('gulp-cssmin');
 
-  if (config.minifyCss === true) {
-    stream.pipe(minifycss());
-  }
+  var sassFiles = gulp.src(appFiles.styles)
+    .pipe(sass({
+      style: isProduction ? 'compressed' : 'expanded',
+      sourcemap: isProduction ? false : true
+    }))
+    .on('error', notify.onError(function (error) {
+      return 'SASS Error: ' + error.message;
+    }))
+    .on('error', function(error){
+      new util.PluginError('CSS', error, {showStack: true});
+    });
 
-  return stream
-    .pipe(gulp.dest('css'))
-    .pipe(notify({ message: 'Successfully compiled LESS' }));
+  return es.concat(gulp.src(vendorFiles.styles), sassFiles)
+    .pipe(concat('kiwifruit.css'))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(isProduction ? combineMediaQueries({log: true}) : util.noop())
+    .pipe(isProduction ? cssmin() : util.noop())
+    .pipe(gulp.dest(paths.dest + 'css'))
+    .pipe(notify({ message: 'SASS compiled' }));
+
 });
 
 // JS
 gulp.task('js', function() {
-  var scripts = [
-    'bower_components/jquery/dist/jquery.js',
-    'bower_components/bootstrap/js/transition.js',
-    'bower_components/bootstrap/js/collapse.js',
-    'bower_components/bootstrap/js/carousel.js',
-    'bower_components/bootstrap/js/dropdown.js',
-    'bower_components/bootstrap/js/modal.js',
-    'src/js/script.js'
-  ];
 
-  if (config.appendLiveReload === true) {
-    scripts.push('src/js/livereload.js');
-  }
+  var jshint = require('gulp-jshint');
+  var uglify = require('gulp-uglify');
 
-  var stream = gulp
-    .src(scripts)
-    .pipe(concat('script.js'));
+  gulp.src(vendorFiles.scripts)
+    .pipe(concat('vendor.js'))
+    .pipe(isProduction ? uglify() : util.noop())
+    .on('error', notify.onError(function (error) {
+      return 'Unglify Error: ' + error.message;
+    }))
+    .pipe(gulp.dest(paths.dest + 'js'));
 
-  if (config.uglifyJS === true) {
-    stream.pipe(uglify());
-  }
+  // App scripts
+  gulp.src(appFiles.scripts)
+    .pipe(concat('kiwifruit.js'))
+    .pipe(isProduction ? uglify() : util.noop())
+    .on('error', notify.onError(function (error) {
+      return 'Unglify Error: ' + error.message;
+    }))
+    .pipe(gulp.dest(paths.dest + 'js'))
+    .pipe(notify({ message: 'JS Compiled' }));
 
-  return stream
-    .pipe(gulp.dest('js'))
-    .pipe(notify({ message: 'Successfully compiled JavaScript' }));
 });
 
 // Images
 gulp.task('images', function() {
-  return gulp
-    .src('src/images/**/*')
-    .pipe(gulp.dest('images'))
-    .pipe(notify({ message: 'Successfully processed image' }));
+  var imagemin = require('gulp-imagemin');
+
+  return gulp.src(appFiles.images)
+    .pipe(imagemin())
+    .on('error', notify.onError(function (error) {
+      return 'Imagemin Error: ' + error.message;
+    }))
+    .pipe(gulp.dest(paths.dest + 'images'))
+    .pipe(notify({ message: 'Images optimized' }));
 });
 
 // Fonts
 gulp.task('fonts', function() {
-  return gulp
-    .src([
-      'bower_components/bootstrap/fonts/**/*',
-      'bower_components/font-awesome/fonts/**/*'
-    ])
-    .pipe(gulp.dest('fonts'))
-    .pipe(notify({ message: 'Successfully processed font' }));
-})
+  return gulp.src(vendorFiles.fonts)
+    .pipe(gulp.dest(paths.dest + 'fonts'));
+});
 
-// Rimraf
-gulp.task('rimraf', function() {
-  return gulp
-    .src(['css', 'js', 'images'], {read: false})
-    .pipe(rimraf());
+// Clean
+gulp.task('clean', function(cb) {
+  require('rimraf')(paths.dest, cb);
 });
 
 // Default task
-gulp.task('default', ['rimraf'], function() {
+gulp.task('default', ['clean'], function() {
     gulp.start('css', 'js', 'images', 'fonts');
 });
 
 // Watch
-gulp.task('watch', function() {
+gulp.task('watch', ['css', 'js', 'images', 'fonts'], function(){
 
-  // Watch .less files
-  gulp.watch('src/less/**/*.less', ['css']);
-
-  // Watch .js files
-  gulp.watch('src/js/**/*.js', ['js']);
-
-  // Watch image files
-  gulp.watch('src/images/**/*', ['images']);
-
-  // Watch fonts
-  gulp.watch('bower_components/bootstrap/fonts/**/*', ['fonts']);
+  gulp.watch(appFiles.styles, ['css']);
+  gulp.watch(appFiles.scripts, ['js']);
+  gulp.watch(appFiles.images, ['images']);
 
   // Create LiveReload server
-  var server = livereload();
+  var server = require('gulp-livereload');
+  server.listen();
 
   // Watch any files in , reload on change
-  gulp.watch(['css/style.css', 'js/script.js', 'images/**/*', 'fonts/**/*']).on('change', function(file) {
-    server.changed(file.path);
-  });
+  gulp.watch(paths.dest + '**/*').on('change', server.changed);
 
 });
---
+++ js/noconflict.js
@@ -0,0 +1,142 @@
+jQuery.noConflict();
+;(function($) {
+    'use strict';
+
+    function Site(settings) {
+
+        this.windowLoaded = false;
+
+    }
+
+    Site.prototype = {
+        constructor: Site,
+
+        start: function() {
+            var me = this;
+
+            $(window).load(function() {
+                me.windowLoaded = true;
+            });
+
+            this.attach();
+        },
+
+        attach: function() {
+            this.attachBootstrapPrototypeCompatibility();
+            this.attachMedia();
+        },
+
+        attachBootstrapPrototypeCompatibility: function() {
+
+            // Bootstrap and Prototype don't play nice, in the sense that
+            // prototype is a really wacky horrible library. It'll
+            // hard-code CSS to hide an element when a hide() event
+            // is fired. See http://stackoverflow.com/q/19139063
+            // To overcome this with dropdowns that are both
+            // toggle style and hover style, we'll add a CSS
+            // class which has "display: block !important"
+            $('*').on('show.bs.dropdown show.bs.collapse', function(e) {
+                $(e.target).addClass('bs-prototype-override');
+            });
+
+            $('*').on('hidden.bs.collapse', function(e) {
+                $(e.target).removeClass('bs-prototype-override');
+            });
+        },
+
+        attachMedia: function() {
+            var $links = $('[data-toggle="media"]');
+            if ( ! $links.length) return;
+
+            // When somebody clicks on a link, slide the
+            // carousel to the slide which matches the
+            // image index and show the modal
+            $links.on('click', function(e) {
+                e.preventDefault();
+
+                var $link = $(this),
+                   $modal = $($link.attr('href')),
+                $carousel = $modal.find('.carousel'),
+                    index = parseInt($link.data('index'));
+
+                $carousel.carousel(index);
+                $modal.modal('show');
+
+                return false;
+            });
+        }
+    };
+
+    jQuery(document).ready(function($) {
+        var site = new Site();
+        site.start();
+    });
+
+})(jQuery);
+
+// Fix the tricky confict between Prototype and jQuery,
+// which cause bugs when hiding various Bootstrap components.
+// See: http://stackoverflow.com/a/20748540/2336096
+(function() {
+  var isBootstrapEvent = false;
+  if (window.jQuery) {
+    var all = jQuery('*');
+    jQuery.each([
+      'hide.bs.dropdown',
+      'hide.bs.collapse',
+      'hide.bs.modal',
+      'hide.bs.tooltip',
+      'hide.bs.popover'
+    ], function(index, eventName) {
+      all.on(eventName, function( event ) {
+        isBootstrapEvent = true;
+      });
+    });
+  }
+  var originalHide = Element.hide;
+  Element.addMethods({
+    hide: function(element) {
+      if(isBootstrapEvent) {
+        isBootstrapEvent = false;
+        return element;
+      }
+      return originalHide(element);
+    }
+  });
+})();
+
+// Fix position issue for iOS when virtual keyborad on.
+jQuery(function($) {
+  // iOS check...ugly but necessary
+  if( navigator.userAgent.match(/iPhone|iPad|iPod/i) ) {
+
+    var  scrollTop;
+
+    $('.modal').on('show.bs.modal', function() {
+      // Position backdrop absolute and make it span the entire page
+      //
+      // Also dirty, but we need to tap into the backdrop after Boostrap
+      // positions it but before transitions finish.
+      //
+      setTimeout( function() {
+        $('.modal-backdrop').css({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: Math.max(
+            document.body.scrollHeight, document.documentElement.scrollHeight,
+            document.body.offsetHeight, document.documentElement.offsetHeight,
+            document.body.clientHeight, document.documentElement.clientHeight
+          ) + 'px'
+        });
+      }, 0);
+
+    }).on('focus', 'input', function() {
+      scrollTop = $(window).scrollTop();
+    }) .on('blur', 'input', function() {
+      $(window).scrollTop(scrollTop);
+    });
+
+  }
+});
