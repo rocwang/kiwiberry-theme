81cf7c46d213f4835fee703b63c9c23674e86ab6
Roc Wang <roc@moustacherepublic.com>
2014-10-03 23:28:39 +1300

    Check the existence of Product object before using it in JS.

--- skin/frontend/default/js/_custom_options.js
+++ skin/frontend/default/js/_custom_options.js
@@ -59,6 +59,7 @@ var optionFileUpload = {
     $('option_' + optionId + '_file_box').remove();
   }
 }
+
 var optionTextCounter = {
   count: function (field, cntfield, maxlimit) {
     if (field.value.length > maxlimit) {
@@ -69,75 +70,79 @@ var optionTextCounter = {
   }
 }
 
-Product.Options = Class.create();
-Product.Options.prototype = {
-  initialize : function (config) {
-    this.config = config;
-    this.reloadPrice();
-    document.observe("dom:loaded", this.reloadPrice.bind(this));
-  },
-  reloadPrice: function () {
-    var config = this.config;
-    var skipIds = [];
-    $$('body .product-custom-option').each(function (element) {
-      var optionId = 0;
-      element.name.sub(/[0-9]+/, function (match) {
-        optionId = parseInt(match[0], 10);
-      });
-      if (config[optionId]) {
-        var configOptions = config[optionId];
-        var curConfig = {price: 0};
-        if (element.type == 'checkbox' || element.type == 'radio') {
-          if (element.checked) {
-            if (typeof configOptions[element.getValue()] != 'undefined') {
-              curConfig = configOptions[element.getValue()];
+if (typeof Product !== 'undefined') {
+
+  Product.Options = Class.create();
+  Product.Options.prototype = {
+    initialize : function (config) {
+      this.config = config;
+      this.reloadPrice();
+      document.observe("dom:loaded", this.reloadPrice.bind(this));
+    },
+    reloadPrice: function () {
+      var config = this.config;
+      var skipIds = [];
+      $$('body .product-custom-option').each(function (element) {
+        var optionId = 0;
+        element.name.sub(/[0-9]+/, function (match) {
+          optionId = parseInt(match[0], 10);
+        });
+        if (config[optionId]) {
+          var configOptions = config[optionId];
+          var curConfig = {price: 0};
+          if (element.type == 'checkbox' || element.type == 'radio') {
+            if (element.checked) {
+              if (typeof configOptions[element.getValue()] != 'undefined') {
+                curConfig = configOptions[element.getValue()];
+              }
             }
-          }
-        } else if (element.hasClassName('datetime-picker') && !skipIds.include(optionId)) {
-          dateSelected = true;
-          $$('.product-custom-option[id^="options_' + optionId + '"]').each(function (dt) {
-            if (dt.getValue() == '') {
-              dateSelected = false;
-            }
-          });
-          if (dateSelected) {
-            curConfig = configOptions;
-            skipIds[optionId] = optionId;
-          }
-        } else if (element.type == 'select-one' || element.type == 'select-multiple') {
-          if ('options' in element) {
-            $A(element.options).each(function (selectOption) {
-              if ('selected' in selectOption && selectOption.selected) {
-                if (typeof(configOptions[selectOption.value]) != 'undefined') {
-                  curConfig = configOptions[selectOption.value];
-                }
+          } else if (element.hasClassName('datetime-picker') && !skipIds.include(optionId)) {
+            dateSelected = true;
+            $$('.product-custom-option[id^="options_' + optionId + '"]').each(function (dt) {
+              if (dt.getValue() == '') {
+                dateSelected = false;
               }
             });
-          }
-        } else {
-          if (element.getValue().strip() != '') {
-            curConfig = configOptions;
-          }
-        }
-        if (element.type == 'select-multiple' && ('options' in element)) {
-          $A(element.options).each(function (selectOption) {
-            if (('selected' in selectOption) && typeof(configOptions[selectOption.value]) != 'undefined') {
-              if (selectOption.selected) {
-                curConfig = configOptions[selectOption.value];
-              } else {
-                curConfig = {price: 0};
-              }
-              optionsPrice.addCustomPrices(optionId + '-' + selectOption.value, curConfig);
-              optionsPrice.reload();
+            if (dateSelected) {
+              curConfig = configOptions;
+              skipIds[optionId] = optionId;
             }
-          });
-        } else {
-          optionsPrice.addCustomPrices(element.id || optionId, curConfig);
-          optionsPrice.reload();
+          } else if (element.type == 'select-one' || element.type == 'select-multiple') {
+            if ('options' in element) {
+              $A(element.options).each(function (selectOption) {
+                if ('selected' in selectOption && selectOption.selected) {
+                  if (typeof(configOptions[selectOption.value]) != 'undefined') {
+                    curConfig = configOptions[selectOption.value];
+                  }
+                }
+              });
+            }
+          } else {
+            if (element.getValue().strip() != '') {
+              curConfig = configOptions;
+            }
+          }
+          if (element.type == 'select-multiple' && ('options' in element)) {
+            $A(element.options).each(function (selectOption) {
+              if (('selected' in selectOption) && typeof(configOptions[selectOption.value]) != 'undefined') {
+                if (selectOption.selected) {
+                  curConfig = configOptions[selectOption.value];
+                } else {
+                  curConfig = {price: 0};
+                }
+                optionsPrice.addCustomPrices(optionId + '-' + selectOption.value, curConfig);
+                optionsPrice.reload();
+              }
+            });
+          } else {
+            optionsPrice.addCustomPrices(element.id || optionId, curConfig);
+            optionsPrice.reload();
+          }
         }
-      }
-    });
+      });
+    }
   }
+
 }
 
 function validateOptionsCallback(elmId, result) {
