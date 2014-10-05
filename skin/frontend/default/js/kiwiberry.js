f4781864df0684a0697124515244ae98d1dda00a
Roc Wang <roc@moustacherepublic.com>
2014-10-06 09:08:19 +1300

    Build billing section of checkout  page.

--- skin/frontend/default/js/kiwiberry.js
+++ skin/frontend/default/js/kiwiberry.js
@@ -216,10 +216,10 @@ jQuery(function ($) {
       sectionElement.addClass('allow');
       this.accordion.openSection('opc-' + section);
 
-      // Scroll viewport to top of checkout steps for smaller viewports
-      if (Modernizr.mq('(max-width: ' + bp.xsmall + 'px)')) {
-        $('html,body').animate({scrollTop: $('#checkoutSteps').offset().top}, 800);
-      }
+      // FIXME: Scroll viewport to top of checkout steps for smaller viewports
+      //if (Modernizr.mq('(max-width: ' + bp.xsmall + 'px)')) {
+      //  $('html,body').animate({scrollTop: $('#checkoutSteps').offset().top}, 800);
+      //}
 
       if (!reloadProgressBlock) {
         this.resetPreviousSteps();
@@ -274,5 +274,78 @@ jQuery(function ($) {
 
   // What's "Remember Me"
   $('.what-is-remember-me').popover();
+
+  if (document.getElementById('customer-dob')) {
+    var customer_dob = new Varien.DOB(
+      '#customer-dob',
+      $('#customer-dob').data('is-required'),
+      $('#customer-dob').data('date-format')
+    );
+  }
+
+  // Button "Save in address book" in checkout page
+  $('#billing\\:save_in_address_book').change(function () {
+    if(window.shipping) shipping.setSameAsBilling(false);
+  });
+
+  // Button "Ship to this address" in checkout page
+  $('#billing\\:use_for_shipping_yes').click(function () {
+    $('#shipping\\:same_as_billing').prop('checked', true);
+  });
+
+  // Button "Ship to different address" in checkout page
+  $('#billing\\:use_for_shipping_no').click(function () {
+    $('#shipping\\:same_as_billing').prop('checked', false);
+  });
+
+  // Billing section in checkout page
+  if (document.getElementById('co-billing-form')) {
+
+    var billing = new Billing(
+      'co-billing-form',
+      $('#co-billing-form').data('address-url'),
+      $('#co-billing-form').data('save-url')
+    );
+    var billingForm = new VarienForm('co-billing-form');
+
+    if (document.getElementById('billing-address-select')) {
+      billing.newAddress(!$('#billing-address-select').val());
+    }
+
+    var billingRegionUpdater = new RegionUpdater(
+      'billing:country_id',
+      'billing:region',
+      'billing:region_id',
+      $('#co-billing-form').data('regions'),
+      undefined,
+      'billing:postcode'
+    );
+
+    if (document.getElementById('onepage-guest-register-button')) {
+
+      $('#onepage-guest-register-button').click(function () {
+
+        var billingRememberMe = $('#co-billing-form .remember-me-box');
+        if (billingRememberMe.length > 0) {
+
+          if ($('#login\\:guest').length > 0 && $('#login\\:guest').prop('checked')) {
+
+            billingRememberMe.hide();
+
+          } else if ($('#login\\:register').length > 0
+            && ($('#login\\:register').prop('checked') || $('#login\\:register').prop('type') === 'hidden')
+          ) {
+
+            billingRememberMe.show();
+
+          }
+        }
+
+      });
+
+    }
+
+  }
+
 });
 
