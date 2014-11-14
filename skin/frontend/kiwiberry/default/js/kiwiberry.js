f0decf6bed3aeeda665090526e1c956bfe9041d2
Roc Wang <me@rocwang.me>
2014-11-14 23:25:41 +1300

    Build "Compare Products" page.

--- skin/frontend/kiwiberry/default/js/kiwiberry.js
+++ skin/frontend/kiwiberry/default/js/kiwiberry.js
@@ -685,5 +685,31 @@ jQuery(function ($) {
   if (document.getElementById('form-validate-reset-password')) {
     var dataForm = new VarienForm('form-validate-reset-password', true);
   }
+
+  // Products comparison page
+  $('.js-print').click(function () {
+    window.print();
+  });
+
+  $('.js-close').click(function () {
+    window.close();
+  });
+
+  $('.js-comparison-remove').click(function (e) {
+    e.preventDefault();
+
+    var column = $(this).closest('td').index() + 1;
+
+    $.ajax($(this).attr('href'), {
+      beforeSend: function () {
+        $('#compare-list-please-wait').show();
+      },
+      data      : {isAjax: 1}
+    }).done(function () {
+      $('#compare-list-please-wait').hide();
+      $('col, td', '#product_comparison').remove(':nth-child('+column+')');
+    });
+  });
+
 });
 
