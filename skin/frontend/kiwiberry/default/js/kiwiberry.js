607c7c045e49e39aec13e3c9df54768e3d9aafa1
Roc Wang <me@rocwang.me>
2015-01-11 23:13:35 +1300

    Build notices and messages.

--- skin/frontend/kiwiberry/default/js/kiwiberry.js
+++ skin/frontend/kiwiberry/default/js/kiwiberry.js
@@ -998,6 +998,20 @@ jQuery(function ($) {
     var swatchesConfig = new Product.ConfigurableSwatches(window.spConfig);
   }
 
-});
+  // Cookie Notice
+  $('#allow-save-cookie').click(function(e) {
+    var button = $(this);
+    var cookieName = button.data('cookie-name');
+    Mage.Cookies.set(
+      cookieName,
+      JSON.stringify(button.data('cookie-value')),
+      new Date(new Date().getTime() + button.data('cookie-expires') * 1000)
+    );
 
+    if (Mage.Cookies.get(cookieName)) {
+      e.preventDefault();
+      window.location.reload();
+    }
+  });
+});
 
