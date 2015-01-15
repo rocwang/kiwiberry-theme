b77f45eb6f8da34f769ee0d0f479483f817c09f7
Roc Wang <me@rocwang.me>
2015-01-16 00:45:43 +1300

    Add doc block to class VR_Kiwiberry_Block_ConfigurableSwatches_Catalog_Layer_State_Swatch.

--- app/code/community/VR/Kiwiberry/Block/ConfigurableSwatches/Catalog/Layer/State/Swatch.php
+++ app/code/community/VR/Kiwiberry/Block/ConfigurableSwatches/Catalog/Layer/State/Swatch.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * Class VR_Kiwiberry_Block_ConfigurableSwatches_Catalog_Layer_State_Swatch
+ */
 class VR_Kiwiberry_Block_ConfigurableSwatches_Catalog_Layer_State_Swatch extends Mage_ConfigurableSwatches_Block_Catalog_Layer_State_Swatch
 {
+    /**
+     * @inheritdoc
+     */
     protected function _init($filter)
     {
         parent::_init($filter);
